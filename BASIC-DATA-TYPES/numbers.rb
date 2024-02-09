658034b61e4cdaec130fd341266098efc6a15626
Ulises Cappa <ulisescappa@gmail.com>
2024-02-09 11:36:38 -0300

    Finish basic numbers description

--- BASIC-DATA-TYPES/numbers.rb
+++ BASIC-DATA-TYPES/numbers.rb
@@ -27,3 +27,22 @@
 17 / 5    #=> 3, not 3.4
 
 17 / 5.0  #=> 3.4
+
+# Converting between integers and floats
+
+# To convert an integer to a float:
+13.to_f   #=> 13.0
+
+# To convert a float to an integer:
+13.0.to_i #=> 13
+13.9.to_i #=> 13
+
+
+# Some useful number methods
+# There are many useful methods for numbers built into Ruby. For example,
+
+6.even? #=> true
+7.even? #=> false
+
+6.odd? #=> false
+7.odd? #=> true
