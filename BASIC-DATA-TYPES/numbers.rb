846c79df6c1c50d0823eee25c3a17f32faa49b7d
Ulises Cappa <ulisescappa@gmail.com>
2024-02-09 11:35:15 -0300

    Inizialize deep-ruby repository. Adding basic data types folder. Add numbers description

---
+++ BASIC-DATA-TYPES/numbers.rb
@@ -0,0 +1,29 @@
+# Addition
+1 + 1   #=> 2
+
+# Subtraction
+2 - 1   #=> 1
+
+# Multiplication
+2 * 2   #=> 4
+
+# Division
+10 / 5  #=> 2
+
+# Exponent
+2 ** 2  #=> 4
+3 ** 4  #=> 81
+
+# Modulus (find the remainder of division)
+8 % 2   #=> 0  (8 / 2 = 4; no remainder)
+10 % 4  #=> 2  (10 / 4 = 2 with a remainder of 2)
+
+
+# Integers and floats
+# There are two main types of numbers in Ruby. Integers are whole numbers, such as 10. Floats are numbers that contain a decimal point, such as 10.5, 10.0, or 0.25.
+
+# It’s important to keep in mind that when doing arithmetic with two integers in Ruby, the result will always be an integer.
+
+17 / 5    #=> 3, not 3.4
+
+17 / 5.0  #=> 3.4
